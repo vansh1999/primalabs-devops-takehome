51cfdab4233f6966d8f02e8cf290a689a86368cf
vansh1999 <vansh.bhardwaj1999@gmail.com>
2026-07-20 22:15:19 +0530

    feat: complete infrastructure, monitoring and CI via github actions workflows

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,6 @@ module "monitoring" {
 
   namespace = var.monitoring_namespace
 
-  metrics_server_chart_version         = var.metrics_server_chart_version
-  kube_prometheus_stack_chart_version  = var.kube_prometheus_stack_chart_version
+  metrics_server_chart_version        = var.metrics_server_chart_version
+  kube_prometheus_stack_chart_version = var.kube_prometheus_stack_chart_version
 }
